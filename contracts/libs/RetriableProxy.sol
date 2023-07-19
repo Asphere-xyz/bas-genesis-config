49751dbe046fa05f80d1444f87556924f9ab24a2
gladcow <s.gladkov@ankr.com>
2023-07-19 18:26:33 +0300

    add helper contracts

---
+++ contracts/libs/RetriableProxy.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/proxy/Proxy.sol";
+
+abstract contract RetryableProxy is Proxy {
+
+    error MethodNotFound();
+
+    function _delegate(address implementation) internal override {
+        assembly {
+        // call contract and store result at 0 address
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+        // check possible results
+            switch result
+            case 0 {
+            // if return code equal to keccak256("MethodNotFound()") then don't revert
+                if iszero(eq(shr(0xe0, mload(0)), 0x06c78984)) {
+                    revert(0, returndatasize())
+                }
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    function _fallback() internal virtual override {
+        // if you override this method then revert must exist in the end of the method
+        revert MethodNotFound();
+    }
+
+    function _implementation() internal pure override returns (address) {
+        return 0x0000000000000000000000000000000000000000;
+    }
+}
